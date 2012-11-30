00ca0226e94066d29566d2db1d9c27e91dd05ea0
yanly <yanly.wang@psu.edu>
2012-11-30 16:26:48 -0500

    update

--- peak-calling/config.sh
+++ peak-calling/config.sh
@@ -1,51 +1,57 @@
 set -ue
 
 OLD_DIR='/home/ywang/retina/fastq/'
-E17_K27_FASTQ_FILENAME='E17_H3K27me3_fastq_prefilter.fq'
-PN15_K27_FASTQ_FILENAME='PN15_H3K27me3_fastq_prefilter.fq'
-E17_K4_FASTQ_FILENAME='E17_H3K4me2_fastq_prefilter_1.3.fq'
-PN15_K4_FASTQ_FILENAME='PN15_H3K4me2_fastq_prefilter_1.3.fq'
+
+E17_K27_BASENAME='E17_H3K27me3'
+PN15_K27_BASENAME='PN15_H3K27me3'
+E17_K4_BASENAME='E17_H3K4me2'
+PN15_K4_BASENAME='PN15_H3K4me2'
+
+E17_K27_FASTQ_FILENAME="${E17_K27_BASENAME}_fastq_prefilter.fq"
+PN15_K27_FASTQ_FILENAME="${PN15_K27_BASENAME}_fastq_prefilter.fq"
+E17_K4_FASTQ_FILENAME="${E17_K4_BASENAME}_fastq_prefilter_1.3.fq"
+PN15_K4_FASTQ_FILENAME="${PN15_K4_BASENAME}_fastq_prefilter_1.3.fq"
 
 E17_K4_OLD_FILE='/home/ywang/retina/H3K4me2_zip/E17_fastq.txt'
 PN15_K4_OLD_FILE='/home/ywang/retina/H3K4me2_zip/PN15_fastq.txt'
 
+FASTQ_DIR="${PWD}/fastq/"
+
+###########################################################################################
 #echo 'Convert quality scores'
 #python ./scripts/convert_quality.py $E17_K4_OLD_FILE "${OLD_DIR}/${E17_K4_FASTQ_FILENAME}"
 #python ./scripts/convert_quality.py $PN15_K4_OLD_FILE "${OLD_DIR}/${PN15_K4_FASTQ_FILENAME}"
 
-FASTQ_DIR="${PWD}/fastq/"
-echo 'Create a fastq folder'
-mkdir -p $FASTQ_DIR
+#echo 'Create a fastq folder'
+#mkdir -p $FASTQ_DIR
 
+#echo 'Copy all files to ${FASTQ_DIR}'
 #FASTQ_FILENAME=$E17_K27_FASTQ_FILENAME
 #cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILENAME=$PN15_K27_FASTQ_FILENAME
-#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILENAME=$E17_K4_FASTQ_FILENAME
-#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILENAME=$PN15_K4_FASTQ_FILENAME
-#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-
-FASTQ_FILE="${FASTQ_DIR}${E17_K27_FASTQ_FILENAME}"
+E17_K27_FASTQ_FILE="${FASTQ_DIR}${E17_K27_FASTQ_FILENAME}"
+FASTQ_FILE=$E17_K27_FASTQ_FILENAME
 OUTPUT_NAME="${FASTQ_FILE%.*}"
-python ./scripts/convert_bases.py $FASTQ_FILE "${OUTPUT_NAME}b.fq"
+#python ./scripts/convert_bases.py $FASTQ_FILE "${OUTPUT_NAME}b.fq"
 E17_K27_FASTQ_FILENAME="${OUTPUT_NAME}b.fq"
 
 FASTQ_FILENAME=$PN15_K27_FASTQ_FILENAME
-cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILE="${FASTQ_DIR}/${FASTQ_FILENAME}"
+#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
 
 FASTQ_FILENAME=$E17_K4_FASTQ_FILENAME
-cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILE="${FASTQ_DIR}/${FASTQ_FILENAME}"
+#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
 
 FASTQ_FILENAME=$PN15_K4_FASTQ_FILENAME
-cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
-#FASTQ_FILE="${FASTQ_DIR}/${FASTQ_FILENAME}"
+#cp -u "${OLD_DIR}${FASTQ_FILENAME}" $FASTQ_DIR
+
+E17_K27_FASTQ_FILE="${FASTQ_DIR}${E17_K27_FASTQ_FILENAME}"
+PN15_K27_FASTQ_FILE="${FASTQ_DIR}${PN15_K27_FASTQ_FILENAME}"
+E17_K4_FASTQ_FILE="${FASTQ_DIR}${E17_K4_FASTQ_FILENAME}"
+PN15_K4_FASTQ_FILE="${FASTQ_DIR}${PN15_K4_FASTQ_FILENAME}"
 
 #trim_con, trim_var, discard
 #QC_METHOD='trim_var'
 SEQTK_CMD='/home/ywang/src/seqtk/seqtk'
 FASTX_CMD='/home/export/tools/fastx/fastq_quality_filter'
 
-#bash quality_control.sh $FASTQ_FILE $QC_METHOD $SEQTK_CMD $FASTX_CMD 50
+bash quality_control.sh $E17_K27_FASTQ_FILE trim_con $SEQTK_CMD $FASTX_CMD .
+FASTQ_FILE="${E17_K27_FASTQ_FILE%.*}_trim30.fq"
